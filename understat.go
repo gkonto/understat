286329bc73586a89f1b4119d9a9ebf62426c6f4a
Georgios Kontogiannis <geor.kontogiannis@gmail.com>
2024-11-17 17:32:45 +0200

    The api must return a copy of cached
    I did this in order to maintain the original structure immutable

--- understat.go
+++ understat.go
@@ -15,13 +15,22 @@ func New() *UnderstatAPI {
 }
 
 func (p *UnderstatAPI) GetPlayers(league model.League, year model.Year) (model.Players, error) {
-	return p.ctrl.GetPlayers(league, year)
+	orig, error := p.ctrl.GetPlayers(league, year)
+	result := make(model.Players, len(orig))
+	copy(result, orig)
+	return result, error
 }
 
 func (p *UnderstatAPI) GetGames(league model.League, year model.Year) (model.Games, error) {
-	return p.ctrl.GetGames(league, year)
+	orig, error := p.ctrl.GetGames(league, year)
+	result := make(model.Games, len(orig))
+	copy(result, orig)
+	return result, error
 }
 
 func (p *UnderstatAPI) GetTeams(league model.League, year model.Year) (model.Teams, error) {
-	return p.ctrl.GetTeams(league, year)
+	orig, error := p.ctrl.GetTeams(league, year)
+	result := make(model.Teams, len(orig))
+	copy(result, orig)
+	return result, error
 }
